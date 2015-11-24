579cdba3819d39ff3ea9bca115a3db590f4b5928
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2015-11-24 10:25:39 +0100

    Added git aliases

--- vagrant/user-config.sh
+++ vagrant/user-config.sh
@@ -1,7 +1,24 @@
 #!/bin/bash -xe
 
+# Configure git
 git config --global user.email "roberto@vasquez-angel.de"
 git config --global user.name "Roberto Vasquez Angel"
+
+tee ~/.gitconfig << OUT
+[alias]
+    co = checkout
+    ec = config --global -e
+    up = !git pull --rebase --prune $@ && git submodule update --init --recursive
+    cob = checkout -b
+    cm = !git add -A && git commit -m
+    save = !git add -A && git commit -m 'SAVEPOINT'
+    wip = !git add -u && git commit -m "WIP" 
+    undo = reset HEAD~1 --mixed
+    amend = commit -a --amend
+    wipe = !git add -A && git commit -qm 'WIPE SAVEPOINT' && git reset HEAD~1 --hard
+    bclean = "!f() { git branch --merged ${1-master} | grep -v " ${1-master}$" | xargs -r git branch -d; }; f"
+    bdone = "!f() { git checkout ${1-master} && git up && git bclean ${1-master}; }; f"
+OUT
 
 # Install RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
